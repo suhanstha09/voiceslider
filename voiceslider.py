0af1f3ebae69e8354498072c4ca71dc0fb5be7e4
suhanstha09 <scubashrestha4@gmail.com>
2026-03-07 21:22:17 +0545

    initialize audio stream with PyAudio configuration

--- voiceslider.py
+++ voiceslider.py
@@ -8,3 +8,12 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 MAX_VOLUME = 3000   
+
+p = pyaudio.PyAudio()
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=CHUNK
+)
